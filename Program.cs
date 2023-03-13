c1a6ca50e97c61d66bba1c0a2924211db4d7b622
Davi Zappala <94610458+Davizappala@users.noreply.github.com>
2023-03-13 20:25:07 -0300

    Add files

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using atvAED3;
+
+namespace atvAED3
+{
+    internal class Funcionario
+    {
+        static void Main(string[] args)
+        {
+
+
+            string nome;
+            double salarioFixo;
+            double totalVendas;
+            double totalRecebido;
+
+            Console.WriteLine("digite o nome do vendedor");
+            nome = Console.ReadLine();
+
+
+            Console.WriteLine("digite o salario do vendedor");
+            salarioFixo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("digite o total de vendas em reais do vendedor");
+            totalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            grana money = new grana();
+            money.totalVendas = totalVendas;
+            money.salarioFixo = salarioFixo;
+
+            totalRecebido = money.Grana();
+            Console.WriteLine("o total recebido foi: " + totalRecebido.ToString("F2"));
+        }
+    }
+}
+
---
+++ grana.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atvAED3
+{
+    public class grana
+    {
+        public string nome;
+        public double salarioFixo;
+        public double totalVendas;
+
+        public double Grana()
+        {
+            return (totalVendas * 0.15) + salarioFixo;
+        }
+    }
+}
